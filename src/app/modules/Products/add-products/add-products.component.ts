fe7ac4488074e09e003b41bfdd65063957268504
mohmed899 <muhammedkhaled889@gmail.com>
2024-07-12 21:17:47 +0300

    fix pro

--- src/app/modules/Products/add-products/add-products.component.ts
+++ src/app/modules/Products/add-products/add-products.component.ts
@@ -8,37 +8,43 @@ import { ToastrService } from 'ngx-toastr';
 import { ProductsService } from '../../../shared/service/products/products.service';
 
 @Component({
-  selector: 'app-add-products',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, DropdownModule],
-  templateUrl: './add-products.component.html',
-  styleUrl: './add-products.component.css'
+	selector: 'app-add-products',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, DropdownModule],
+	templateUrl: './add-products.component.html',
+	styleUrl: './add-products.component.css'
 })
 export class AddProductsComponent {
 
-  categories: any;
-  brands: any;
+	categories: any;
+	brands: any;
 	productForm!: FormGroup;
 	product: any;
 	submitted: boolean = false;
 	constructor(
 		private route: ActivatedRoute,
 		private productService: ProductsService,
-    private categoriesService: CategoriesService,
+		private categoriesService: CategoriesService,
 		private fb: FormBuilder,
 		private toastr: ToastrService,
 		private router: Router
 	) { }
 	id!: number;
+	tags:any
 	ngOnInit(): void {
 		this.initForm();
 		this.getAllCategories();
-    this.getAllBrands();
+		this.getAllBrands();
 		this.route.params.subscribe(res => {
 			this.id = res["id"];
 			if (this.id)
 				this.getProductByid(this.id);
 		});
+     this.tags =[
+		{id:0,name:'Most Popular'},
+		{id:1,name:'Recent'},
+		{id:2,name:'Best'}
+	 ]		
 	}
 
 	getAllCategories() {
@@ -46,11 +52,11 @@ export class AddProductsComponent {
 			this.categories = res;
 		})
 	}
-  getAllBrands(){
-    this.productService.getAllBrands().subscribe(res => {
+	getAllBrands() {
+		this.productService.getAllBrands().subscribe(res => {
 			this.brands = res;
 		})
-  }
+	}
 
 	initForm() {
 		this.productForm = this.fb.group({
@@ -59,17 +65,17 @@ export class AddProductsComponent {
 			description: ["", Validators.required],
 			descriptionAr: ["", Validators.required],
 			categoryId: [null, Validators.required],
-      brandId: [null, Validators.required],
-      price: [null, [Validators.required, Validators.min(1)]],
-      qty: [null, [Validators.required, Validators.min(1)]],
-      discountPercentage: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
-      tag: [0, Validators.required]
+			brandId: [null, Validators.required],
+			price: [null, [Validators.required, Validators.min(1)]],
+			qty: [null, [Validators.required, Validators.min(1)]],
+			discountPercentage: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+			tag: [null]
 		})
 	}
-  
+
 	getProductByid(id: number) {
 		this.productService.getProductByid(id).subscribe(res => {
-			this.product= res;
+			this.product = res;
 			this.productForm.patchValue(this.product);
 		})
 	}
@@ -78,6 +84,9 @@ export class AddProductsComponent {
 	SaveData() {
 		const brandId = this.productForm.get("brandId")?.getRawValue();
 		if (brandId) this.productForm.get("parentId")?.patchValue(+brandId);
+
+		const tag = this.productForm.get("tag")?.getRawValue();
+		if (tag) this.productForm.get("tag")?.patchValue(+tag);
 		if (this.productForm.invalid) {
 			this.submitted = true;
 			return;
